60bccaa6d9bfdc44035eec875385dd88245d8987
Martin Lindström <martinllindstroem@gmail.com>
2023-11-20 16:41:57 +0100

    Create routes for API

---
+++ backend/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load("./.env")
+
+	portString := os.Getenv("PORT")
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options {
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Get("/movies", handlerGetMovies)
+	v1Router.Post("/movies/add", handlerAddMovie)
+	v1Router.Delete("/movies/delete", handlerDeleteMovie)
+	v1Router.Put("/movies/edit", handlerEditMovie)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server {
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port: ", portString)
+
+}
