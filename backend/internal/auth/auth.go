a0d8c6944e6f2b971b6be28028f5aedb5e0e3b2b
Martin Lindström <martinllindstroem@gmail.com>
2023-12-04 11:57:26 +0100

    add method to authenticate user

--- backend/internal/auth/auth.go
+++ backend/internal/auth/auth.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -28,7 +29,7 @@ func CreateJwtToken(username string) (string, error) {
     
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims {
         "username": username,
-        "exp": time.Now().Add(time.Minute * 30),
+        "exp": time.Now().Add(time.Minute * 30).Unix(),
     })
 
     tokenString, err := token.SignedString(jwtKey)
@@ -54,4 +55,24 @@ func VerifyJwtToken(tokenString string) error {
     }
 
     return nil
+}
+
+func AuthorizeUser(r *http.Request) (msg string, code int, err error) {
+    c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return "Unauthorized", 401, err
+		}
+
+		return "An error occured",400, err
+	}
+
+	tokenString := c.Value
+	err = VerifyJwtToken(tokenString)
+
+	if err != nil {
+        return "Unauthorized", 401, err
+	}
+
+    return "Successful login", 200, nil
 }
