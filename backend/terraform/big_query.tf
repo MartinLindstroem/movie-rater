ae4b2d91bf6859b7de41c7a36e49d1221ed58f1f
Martin Lindström <martinllindstroem@gmail.com>
2023-12-12 14:29:19 +0100

    add bigquery and restructure terraform files

---
+++ backend/terraform/big_query.tf
@@ -0,0 +1,89 @@
+resource "google_pubsub_topic" "movies" {
+  name = "movie-topic"
+}
+
+resource "google_pubsub_topic" "pageviews" {
+  name = "pageview-topic"
+}
+
+resource "google_pubsub_subscription" "movies" {
+  name  = "movies-subscription"
+  topic = google_pubsub_topic.movies.name
+
+  bigquery_config {
+    table = "${google_bigquery_table.movies.project}.${google_bigquery_table.movies.dataset_id}.${google_bigquery_table.movies.table_id}"
+  }
+
+#   depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
+}
+
+resource "google_pubsub_subscription" "pageviews" {
+  name  = "pageviews-subscription"
+  topic = google_pubsub_topic.pageviews.name
+
+  bigquery_config {
+    table = "${google_bigquery_table.pageviews.project}.${google_bigquery_table.pageviews.dataset_id}.${google_bigquery_table.pageviews.table_id}"
+  }
+
+#   depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
+}
+
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "the-maze-go"
+  friendly_name               = "maze-data"
+  description                 = "Dataset for logging page views and movie actions"
+  location                    = "europe-north1"
+}
+
+resource "google_bigquery_table" "movies" {
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = "movies"
+
+  schema = <<EOF
+[
+  {
+    "name": "title",
+    "type": "STRING",
+    "mode": "REQUIRED",
+    "description": "Movie title"
+  },
+  {
+    "name": "date",
+    "type": "TIMESTAMP",
+    "mode": "REQUIRED",
+    "description": "Date when movie was added"
+  }
+]
+EOF
+
+}
+
+resource "google_bigquery_table" "pageviews" {
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = "page-views"
+
+  schema = <<EOF
+[
+  {
+    "name": "path",
+    "type": "STRING",
+    "mode": "REQUIRED",
+    "description": "Path of the accessed page"
+  },
+  {
+    "name": "userAgent",
+    "type": "string",
+    "mode": "REQUIRED",
+    "description": "Device used to access the page"
+  },
+  {
+    "name": "date",
+    "type": "TIMESTAMP",
+    "mode": "REQUIRED",
+    "description": "Timestamp of when the page was accessed"
+  }
+]
+EOF
+
+}
---
+++ backend/terraform/cloud_run.tf
@@ -0,0 +1,31 @@
+
+# Create backend cloud run instance
+resource "google_cloud_run_service" "run_service" {
+    name = "cloudrun-maze-backend"
+    location = var.region
+
+    template {
+        spec {
+            containers {
+                image = "europe-north1-docker.pkg.dev/the-maze-go/the-maze-go/backend:${var.github_sha}"
+                env {
+                name = "PROJECT_ID"
+                value = var.projectID
+                }
+            }
+        }
+    }
+
+    traffic {
+        percent = 100
+        latest_revision = true
+    }
+}
+
+# Allow unauthenticated users to invoke the service
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+    service  = google_cloud_run_service.run_service.name
+    location = google_cloud_run_service.run_service.location
+    role     = "roles/run.invoker"
+    member   = "allUsers"
+}
