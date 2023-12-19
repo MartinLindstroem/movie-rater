2526d6ff72baf2ccf0af4af559d44f728e7752c1
Martin Lindström <martinllindstroem@gmail.com>
2023-12-19 15:45:15 +0100

    add pubsub->bigquery logging

--- backend/handlers/movie_handlers.go
+++ backend/handlers/movie_handlers.go
@@ -1,24 +1,24 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 
 	"github.com/Geshdo/the-maze-go-martin/helpers"
 	"github.com/Geshdo/the-maze-go-martin/internal/auth"
 	"github.com/Geshdo/the-maze-go-martin/internal/database"
+	"github.com/Geshdo/the-maze-go-martin/internal/logging"
 	"github.com/Geshdo/the-maze-go-martin/types"
 )
 
+// func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
+// 	helpers.RespondWithJSON(w, 200, struct{}{})
+// }
 
-
-func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
-	helpers.RespondWithJSON(w, 200, struct{}{})
-}
-
-func HandlerErr(w http.ResponseWriter, r *http.Request) {
-	helpers.RespondWithError(w, 400, "Something went wrong")
-}
+// func HandlerErr(w http.ResponseWriter, r *http.Request) {
+// 	helpers.RespondWithError(w, 400, "Something went wrong")
+// }
 
 func GetMoviesHandler(w http.ResponseWriter, r *http.Request) {
 	movies := database.GetAllMovies()
@@ -28,20 +28,22 @@ func GetMoviesHandler(w http.ResponseWriter, r *http.Request) {
 func AddMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var data types.AddMovieRequest
 	err := json.NewDecoder(r.Body).Decode(&data)
-	
+
 	if err != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, "Error decoding JSON")
 		return
 	}
 
-	if (data.Title == "" || data.Imdb == "" || data.Rating < 1) {
+	if data.Title == "" || data.Imdb == "" || data.Rating < 1 {
 		helpers.RespondWithError(w, 400, "Invalid data given")
 	} else {
 		err = database.AddMovie(data)
 		if err != nil {
 			helpers.RespondWithError(w, 500, "Something went wrong")
-		}  else {
+		} else {
 			helpers.RespondWithJSON(w, 201, "Movie successfully added")
+			var buffer bytes.Buffer
+			logging.PublishTopic(&buffer, "movie-topic", types.TopicMessage{MovieTitle: data.Title})
 		}
 	}
 }
@@ -55,13 +57,13 @@ func DeleteMovieHandler(w http.ResponseWriter, r *http.Request) {
 
 	var data types.DeleteMovieRequest
 	err = json.NewDecoder(r.Body).Decode(&data)
-	
+
 	if err != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, "Error decoding JSON")
 		return
 	}
 
-	if (data.Id == "") {
+	if data.Id == "" {
 		helpers.RespondWithError(w, 400, "Invalid data given")
 	} else {
 		err = database.DeleteMovie(data.Id)
@@ -82,13 +84,13 @@ func EditMovieHandler(w http.ResponseWriter, r *http.Request) {
 
 	var data types.EditMovieRequest
 	err = json.NewDecoder(r.Body).Decode(&data)
-	
+
 	if err != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, "Error decoding JSON")
 		return
 	}
 
-	if (data.Id == "" || data.Rating < 1) {
+	if data.Id == "" || data.Rating < 1 {
 		helpers.RespondWithError(w, 400, "Invalid data given")
 	} else {
 		err = database.EditMovie(data)
@@ -103,13 +105,13 @@ func EditMovieHandler(w http.ResponseWriter, r *http.Request) {
 func RateMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var data types.UpdateRatingRequest
 	err := json.NewDecoder(r.Body).Decode(&data)
-	
+
 	if err != nil {
 		helpers.RespondWithError(w, http.StatusBadRequest, "Error decoding JSON")
 		return
 	}
 
-	if (data.Rating < 1 || data.Id == "") {
+	if data.Rating < 1 || data.Id == "" {
 		helpers.RespondWithError(w, 400, "Invalid data given")
 	} else {
 		err = database.UpdateMovieRating(data)
