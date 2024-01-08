5564faea76b68c9fdf64ef2eeb045e8e9182ad51
Martin Lindström <martinllindstroem@gmail.com>
2024-01-08 13:43:34 +0100

    set security headers

--- backend/helpers/helpers.go
+++ backend/helpers/helpers.go
@@ -27,7 +27,13 @@ func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
-	
+
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubdomains")
+	w.Header().Add("Content-Security-Policy", "default-src 'self'")
+	w.Header().Add("X-XSS-Protection", "1; mode=block")
+	w.Header().Add("X-Frame-Options", "DENY")
+	w.Header().Add("Referrer-Policy", "strict-origin-when-cross-origin")
+	w.Header().Add("X-Content-Type-Options", "nosniff")
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(data)
